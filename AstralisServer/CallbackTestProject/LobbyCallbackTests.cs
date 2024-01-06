7d3703f86835a971a8139d493b5b5d9f8b52aed8
Mario Portilla <mariom.portilla@hotmail.com>
2024-01-06 01:45:23 -0600

    Primera iteración de pruebas de callback

---
+++ AstralisServer/CallbackTestProject/LobbyCallbackTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MessageService;
+
+namespace CallbackTestProject
+{
+    [TestClass]
+    public class LobbyCallbackTests
+    {
+
+        [ClassInitialize]
+        public void Initialize()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMetho()
+        {
+
+        }
+    }
+
+    public class LobbyCallbackImplementation
+    {
+        public bool BeenKicked { get; set; }
+
+        public bool ConnectedToLobby { get; set; }
+
+        public bool ShowConnectionInLobby { get; set; }
+
+        public bool ShowDisconnectionInLobby { get; set; }
+
+        public int TeamChanged { get; set; }
+
+        public string Message { get; set; }
+
+        public bool GameStarted { get; set; }
+
+        public LobbyCallbackImplementation()
+        {
+            BeenKicked = false;
+            ConnectedToLobby = false;
+            ShowConnectionInLobby = false;
+            ShowDisconnectionInLobby = false;
+            TeamChanged = 0;
+            Message = string.Empty;
+            GameStarted = false;
+        }
+
+
+    }
+}
---
+++ AstralisServer/CallbackTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CallbackTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CallbackTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("545e9f2d-d21a-4a7b-bac7-6b8321a63d45")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
